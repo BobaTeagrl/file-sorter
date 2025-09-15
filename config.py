a2f75f250e44d9d9f30739e65cb129e8ae5f405d
BobaTeagrl <Unsorted2750@proton.me>
2025-09-15 16:46:44 -0400

    copy files to new made folders based on config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 source_path = "/home/bobateagrl/Documents/copytest"
-
+destination_path = "/home/bobateagrl/Documents/copytest/test"
+extentions = [".png, .webp"]
--- sorter.py
+++ sorter.py
@@ -4,7 +4,16 @@ import config
 
 
 
-
+destination_root =Path(config.destination_path)
 entries = Path(config.source_path)
+destination = Path(config.destination_path)
+  
 for entry in entries.iterdir():
-    print(entry.name, entry.suffix)
+    if entry.is_file():
+        # make a subfolder 
+        subfolder = destination_root / entry.suffix.lower().lstrip(".")
+        subfolder.mkdir(parents=True, exist_ok=True)
+
+        # copy the file into folder
+        shutil.copy(entry, subfolder)
+        print(f"Copied {entry.name} -> {subfolder}")
